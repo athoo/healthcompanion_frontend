fab575ed9dda4b3264afc01711b21fed2f5184d6
yangpingjing <athoo@sina.com>
2017-02-26 20:41:22 -0600

    add host

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var path = require("path");
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/.'));
+
+// views is directory for all template files
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname+'/index.html'));
+});
+
+
+// app.get('/cool', function(request, response) {
+//   response.send(cool());
+// });
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
